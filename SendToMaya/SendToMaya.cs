ac2ab0d2c72e7dffd6c35ea45eb6d393a36a028d
Mario Latronico <Mario Latronico>
2017-06-13 14:08:47 +0200

    Add project files.

---
+++ SendToMaya/SendToMaya.cs
@@ -0,0 +1,184 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SendToMaya.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using System.Net.Sockets;
+using System.Net;
+
+namespace SendToMaya
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class SendToMaya
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int SendToMayaCmdId = 0x0100;
+        public const int SendToMayaDebugCmdId = 0x0102;
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("dbbae0c6-3fa7-46b9-800c-1afae02535c0");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+
+        private OutputWindowPane outputPane;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SendToMaya"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private SendToMaya(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var sendToMayaMenuCommandID = new CommandID(CommandSet, SendToMayaCmdId);
+                var sendToMayaMenuItem = new MenuCommand(this.SendToMayaCallback, sendToMayaMenuCommandID);
+                commandService.AddCommand(sendToMayaMenuItem);
+
+                var sendToMayaDebugMenuCommandID = new CommandID(CommandSet, SendToMayaDebugCmdId);
+                var sendToMayaDebugMenuItem = new MenuCommand(this.SendToMayaCallback, sendToMayaDebugMenuCommandID);
+                commandService.AddCommand(sendToMayaDebugMenuItem);
+                
+            }
+            outputPane = CreatePane("Maya");
+            
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static SendToMaya Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        OutputWindowPane CreatePane(string title)
+        {
+            EnvDTE80.DTE2 dte = (EnvDTE80.DTE2)ServiceProvider.GetService(typeof(DTE));
+            OutputWindowPanes panes =
+                dte.ToolWindows.OutputWindow.OutputWindowPanes;
+            OutputWindowPane pane;
+            try
+            {
+                // If the pane exists already, write to it.  
+                pane = panes.Item(title);
+            }
+            catch (ArgumentException)
+            {
+                // Create a new pane and write to it.  
+                return panes.Add(title);
+            }
+            return pane;
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new SendToMaya(package);
+        }
+
+        private void SendToMayaCallback(object sender, EventArgs e)
+        {
+            var dte = ServiceProvider.GetService(typeof(_DTE)) as _DTE;
+            if (dte == null) return;
+
+            var activeDocument = dte.ActiveDocument;
+            if (activeDocument == null) return;
+            var textDoc = activeDocument.Object() as TextDocument;
+            if (textDoc == null) return;
+            var text = textDoc.CreateEditPoint(textDoc.StartPoint).GetText(textDoc.EndPoint);
+
+
+            // get the options
+            SendToMayaPackage sendToMayaPackage = this.package as SendToMayaPackage;
+            var port = sendToMayaPackage.OptionPort;
+            var hostname = sendToMayaPackage.OptionHost;
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPHostEntry hostEntry;
+
+            hostEntry = Dns.GetHostEntry(hostname);
+
+            IPAddress ip = null;
+            // get the IPv4 address
+            foreach (IPAddress ipAddr in hostEntry.AddressList)
+            {
+                if (ipAddr.AddressFamily == AddressFamily.InterNetwork)
+                    ip = ipAddr;
+            }
+
+            MenuCommand menuCommand = sender as MenuCommand;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            if (menuCommand.CommandID.ID == SendToMayaDebugCmdId)
+            {
+                //sb.AppendFormat("python(\"");
+                sb.AppendFormat("import ptvsd{0}", Environment.NewLine);
+                sb.AppendFormat("ptvsd.enable_attach(secret='secret', address = ('{0}', {1}){2}", ip.ToString(), sendToMayaPackage.OptionDebugPort, Environment.NewLine);
+                sb.AppendLine("ptvsd.wait_for_attach()");
+              
+            
+            }
+            sb.Append(text);
+            text = sb.ToString();
+
+
+            if (ip != null)
+            {
+                socket.Connect(ip, port);
+
+                byte[] byData = System.Text.Encoding.ASCII.GetBytes(text);
+                System.Console.WriteLine(text);
+                socket.Send(byData);
+                byte[] recvData = new byte[4096];
+                int recvCount = socket.Receive(recvData);
+                string returnString = System.Text.Encoding.ASCII.GetString(recvData, 0, recvCount);
+                
+                System.Console.WriteLine(returnString);
+                outputPane.OutputString(returnString);
+
+                socket.Disconnect(false);
+                socket.Close();
+            }
+        }
+
+    }
+}
---
+++ SendToMaya/SendToMayaPackage.cs
@@ -0,0 +1,146 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SendToMayaPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using System.ComponentModel;
+
+namespace SendToMaya
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(SendToMayaPackage.PackageGuidString)]
+    [ProvideOptionPage(typeof(SendToMayaOptionsPage),  "Send to Maya", "Main Options", 0, 0, true)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class SendToMayaPackage : Package
+    {
+        /// <summary>
+        /// SendToMayaPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "a4d5f9d6-88a4-40ed-96de-e24e43205269";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SendToMaya"/> class.
+        /// </summary>
+        public SendToMayaPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Options page
+        public class SendToMayaOptionsPage : DialogPage
+        {
+            private int port = 7720;
+            private int debugPort = 5678;
+            private string hostname = "localhost";
+            [Category("Send to Maya")]
+            [DisplayName("Port")]
+            [Description("Host port number")]
+            public int OptionPort
+            {
+                get { return port; }
+                set { port = value; }
+            }
+
+            [Category("Send to Maya")]
+            [DisplayName("Debug Port")]
+            [Description("Remote host debug port number")]
+            public int OptionDebugPort
+            {
+                get { return debugPort; }
+                set { debugPort = value; }
+            }
+
+
+            [Category("Send to Maya")]
+            [DisplayName("Hostname")]
+            [Description("Hostname of Maya instance")]
+            public string OptionHost
+            {
+                get { return hostname; }
+                set { hostname = value; }
+            }
+
+
+        }
+        #endregion
+
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            SendToMaya.Initialize(this);
+            base.Initialize();
+        }
+
+        public int OptionPort
+        {
+            get
+            {
+                SendToMayaOptionsPage page = (SendToMayaOptionsPage)GetDialogPage(typeof(SendToMayaOptionsPage));
+                return page.OptionPort;
+            }
+        }
+
+        public int OptionDebugPort
+        {
+            get
+            {
+                SendToMayaOptionsPage page = (SendToMayaOptionsPage)GetDialogPage(typeof(SendToMayaOptionsPage));
+                return page.OptionDebugPort;
+            }
+        }
+
+
+        public string OptionHost
+        {
+            get
+            {
+                SendToMayaOptionsPage page = (SendToMayaOptionsPage)GetDialogPage(typeof(SendToMayaOptionsPage));
+                return page.OptionHost;
+            }
+        }
+
+
+
+        #endregion
+    }
+}
